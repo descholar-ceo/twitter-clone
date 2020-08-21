3f542e80cbe015fb28878527116379008f02a470
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-21 12:25:31 +0200

    Fix rubocop lint errors

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: %i[show edit update destroy]
-  before_action :authenticate_user!, except: [ :index, :show ]
+  before_action :authenticate_user!, except: %i[index show]
 
   # GET /tweets
   # GET /tweets.json
