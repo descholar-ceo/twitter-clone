d6e410d342ec343fb3aa54fe5355d474c24482c6
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-21 12:23:40 +0200

    Add account update params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,4 +5,8 @@ class RegistrationsController < Devise::RegistrationsController
     def sign_up_params
         params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
     end
+
+    def account_update_params
+        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+    end
 end
