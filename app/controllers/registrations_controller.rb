bd90957fba3d45c88f89dc3fdaa75a39cb344b2f
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-21 12:25:03 +0200

    Add current password field

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,6 @@ class RegistrationsController < Devise::RegistrationsController
     end
 
     def account_update_params
-        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
     end
 end
